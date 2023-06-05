8c15061f2ed341ac4283200657568ee373f332a7
buusra <busrakirsac@gmail.com>
2023-06-05 23:16:07 +0300

    ikinci yukleme

--- src/main/java/day20statickeywordconstructors/StdRunner.java
+++ src/main/java/day20statickeywordconstructors/StdRunner.java
@@ -4,14 +4,15 @@ public class StdRunner {
     public static void main(String[] args) {
 
         //stdName static oldugundan ona ulasmak icin object olusturmadim
-        //sadece class ismini kullanmak yeterlidir
+        //sadece class ismini kullanmak yeterlidir. "Student." yazinca ekranda stdName cikiyor.
+        //age cikmiyor cunku static degil. non-staticlere ulasmak icin object olusturmamiz gerek
         System.out.println(Student.stdName); //Tom Hanks
 
         //age non-static oldugundan ona ulasmak icin object olusturmak zorundayiz
-        Student std1 = new Student();
-        System.out.println(std1.age); //13
+        Student std1 = new Student(); // object olusturmak icin gerekli adimlar. Artik non-static uzerinden objectime ulasabilirim
+        System.out.println(std1.age); //13  "std1." yazinca non-static olan age'e ulastim
 
-        Student std2= new Student();
+        Student std2= new Student(); //2.objemi olusturdum
 
         //class name uzerinden staticMethod'a ulastim. Ben static methodum
         Student.staticMethod();
@@ -25,6 +26,7 @@ public class StdRunner {
         System.out.println(std1.stdName); //Tom Hanks
 
         std1.staticMethod();//Ben static methodum
+
 
 
 
--- src/main/java/day20statickeywordconstructors/Student.java
+++ src/main/java/day20statickeywordconstructors/Student.java
@@ -4,7 +4,7 @@ public class Student {
 
     /*
     1) Static variable veya static methodlar (class member) tum object'ler icin ortak elemandir.
-    2) Static class member lar uzerinde yapilan tum degisiklikler, tum objectleri etkiler.
+    2) Static class member lar uzerinde yapilan tum degisiklikler tum objectleri etkiler.
     3) Static class memberlar class'a, non-static class memberlar objectlere monte edilir.
        Mesela; bir class'dan 100 tane object olusturdugunuzda, non-static olanlar 100 kere olusturulur,
        ama static olanlar object sayisindan bagimsiz olarak bir kere olusturulur.
@@ -14,21 +14,21 @@ public class Student {
        Non-static variable'larin diger adi "Instance Variable" veya "Object Variable" dir.
      */
 
-    public static String stdName="Tom Hanks"; //memory de bir alan ayirdim ve buna her yerden ulasabilen static bir variable'a donusturdum
+    public static String stdName = "Tom Hanks"; //memory de bir alan ayirdim ve buna her yerden ulasilabilen static bir variable'a donusturdum
 
-    public int age=13;
+    // String stdName="Tom Hanks";         --> non-static variable olusturdum. Bunu static yapmak istersem basina static koyuyorum
+    // static String stdName="Tom Hanks";  -->static variable. Basina static yazarak class variable'a donusturdum
+
+    public int age = 13; //non-static
 
 
-    public static void staticMethod (){
+    public static void staticMethod() {
         System.out.println("Ben static methodum");
     }
 
-    public void nonStaticMethod(){
+    public void nonStaticMethod() {
         System.out.println("Ben static olmayan methodum");
     }
-
-
-
 
 
 }
